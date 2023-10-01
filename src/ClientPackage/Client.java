0e0eace86eca035cf20e830fb45438889eb09615
MahdiMzid <mzidmahdi1234@gmail.com>
2023-10-01 19:57:55 +0100

    Update Client.java

--- src/ClientPackage/Client.java
+++ src/ClientPackage/Client.java
@@ -1,5 +1,6 @@
 package ClientPackage;
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class Client {
@@ -8,14 +9,30 @@ public class Client {
 
 		try {
 			
-			// La première étape
+			//Connexion
 			int port=1234;
 			InetAddress server= InetAddress.getLocalHost();
 			System.out.println("Je suis un client pas encore connecté...");
 			Socket socket = new Socket(server,port);
 			System.out.println("Je suis un client connecté");
 			
-			// La dernière étape
+			// Lecture d'un entier
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Donner un entier");
+			int x = scanner.nextInt();
+			
+			// Envoie du entier vers le server
+			OutputStream os = socket.getOutputStream();
+			os.write(x);
+			
+			//Reçoit du résultat
+			InputStream is = socket.getInputStream();
+			int result = is.read();
+			
+			//Afficahge du résultat
+			System.out.println("Multiple de "+ x +" est "+result);
+			
+			//Fermeture du Connexion
 			socket.close();
 			
 		} catch (IOException e) {
